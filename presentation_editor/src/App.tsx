b03787085e5003624d5870d933def16bc4ba0b4b
Elena <159633810+skjdhfs@users.noreply.github.com>
2025-12-31 22:43:09 +0300

    lab 7.2

--- presentation_editor/src/App.tsx
+++ presentation_editor/src/App.tsx
@@ -3,12 +3,41 @@ import { Toolbar } from './view/Toolbar/Toolbar';
 import { Sidebar } from './view/Sidebar/Sidebar';
 import { SlideView } from './view/SlideView/SlideView';
 import { NoSlidesPlaceholder } from './view/NoSlidesPlaceholder/NoSlidesPlaceholder';
-import { useAppSelector } from './store/hooks/reduxHooks';
+import { useAppSelector, useAppDispatch } from './store/hooks/reduxHooks';
+import { undoAction, redoAction } from './store/editorSlice';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useAppDispatch()
   const slides = useAppSelector((state) => state.editor.present.presentation.slides);
   const selectedSlidesIds = useAppSelector((state) => state.editor.present.selected.selectedSlidesIds);
   const firstSelectedSlide = slides.find((slide) => slide.id == selectedSlidesIds[0]);
+
+useEffect(() => {
+  const handleKeyDown = (event: KeyboardEvent) => {
+    const isZ = event.code === 'KeyZ'; 
+    const isY = event.code === 'KeyY';
+    const isMod = event.ctrlKey || event.metaKey;
+
+    if (!isMod) return;
+    if (event.repeat) return;
+
+    if (isZ) {
+      event.preventDefault();
+      event.stopPropagation(); 
+      dispatch(undoAction());
+    }
+    
+    if (isY) {
+      event.preventDefault();
+      event.stopPropagation();
+      dispatch(redoAction());
+    }
+  };
+
+  window.addEventListener('keydown', handleKeyDown, { capture: true });
+  return () => window.removeEventListener('keydown', handleKeyDown, { capture: true });
+}, [dispatch]);
 
   let WorkspaceContent;
 
